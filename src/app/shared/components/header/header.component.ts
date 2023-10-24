b156135929a8a8d9aaba3c19c3db2b944bb4ca4d
LaDChoromota <zapaterol06@gmail.com>
2023-10-23 21:15:09 -0300

    a

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +10,16 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent  implements OnInit {
   @Input() title!: string;
   @Input() backButton!: string;
+  utilsSvc = inject(UtilsService)
+  firebaseSvc = inject(FirebaseService)
+
 
   constructor() { }
 
   ngOnInit() {}
+
+  signOut(){
+    this.firebaseSvc.signOut()
+  }
 
 }
